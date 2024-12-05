feead64794722959400bee56255de7f9c9938d1b
Pense.try <pensetry@hotmail.com>
2024-12-05 19:59:49 -0300

    Site 0.0v inicial

--- src/JS/navbar.js
+++ src/JS/navbar.js
@@ -1,6 +1,11 @@
 const menuToggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 
+
+
 menuToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
+  
+  // Alterna o ícone
+  menuToggle.textContent = sidebar.classList.contains('active') ? '✖' : '☰';
 });
