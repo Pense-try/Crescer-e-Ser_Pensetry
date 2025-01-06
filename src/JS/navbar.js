460bcad6e0aefabefa88514454769cae6145ad18
Pense.try <pensetry@hotmail.com>
2025-01-06 17:51:40 -0300

    Site 0.8v adp

--- src/JS/navbar.js
+++ src/JS/navbar.js
@@ -1,11 +1,10 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-
-
-menuToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-  
-  // Alterna o ícone
-  menuToggle.textContent = sidebar.classList.contains('active') ? '✖' : '☰';
-});
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "./src/Img/navbar/menu-aberto.png";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "./src/Img/navbar/excluir.png";
+  }
+}
