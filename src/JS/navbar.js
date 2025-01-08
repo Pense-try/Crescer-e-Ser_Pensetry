2bd7684c5588a732f0b8aee2a25d9c600ef2156f
Pense.try <pensetry@hotmail.com>
2025-01-08 14:52:58 -0300

    Site 0.9v inicial

--- src/JS/navbar.js
+++ src/JS/navbar.js
@@ -2,9 +2,9 @@ function menuShow() {
   let menuMobile = document.querySelector('.mobile-menu');
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
-      document.querySelector('.icon').src = "./src/Img/navbar/menu-aberto.png";
+      document.querySelector('.icon').src = "./src/Img/menu-icon.png";
   } else {
       menuMobile.classList.add('open');
-      document.querySelector('.icon').src = "./src/Img/navbar/excluir.png";
+      document.querySelector('.icon').src = "./src/Img/excluir-icon.png";
   }
 }
